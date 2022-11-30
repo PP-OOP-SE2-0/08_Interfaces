5144a8d86010027c01a4bef53fc9e9916f5e395b
Sabine Eitel <Seit201@aol.com>
2022-11-30 11:07:26 +0100

    packege

---
+++ com/cc/java/bees/Queen.java
@@ -0,0 +1,16 @@
+package com.cc.java.bees;
+
+public class Queen extends HoneyBee{
+
+    @Override
+    public String doYourJob() {
+        return "I'm a Queen, any questions?";
+     }
+
+     @Override
+     public String fly() {
+         return "I believe, I can fly at 10mph!";
+     }
+      
+    
+}
